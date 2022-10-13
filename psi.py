4194823033f7d5095e0e56e74699f7aa389cd62e
unknown <chuduongboss01@gmail.com>
2022-10-13 10:57:25 +0700

    fix psi.py

--- psi.py
+++ psi.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-def calculate_psi(expected, actual, buckettype='bins', buckets=10, axis=0):
+def calculate_psi(expected, actual, breakpoints, axis=0):
     '''Calculate the PSI (population stability index) across all variables
 
     Args:
        expected: numpy matrix of original values
        actual: numpy matrix of new values, same size as expected
-       buckettype: type of strategy for creating buckets, bins splits into even splits, quantiles splits into quantile buckets
-       buckets: number of quantiles to use in bucketing variables
+       breakpoints: list of breakpoints
        axis: axis by which variables are defined, 0 for vertical, 1 for horizontal
 
     Returns:
@@ -19,13 +18,13 @@ def calculate_psi(expected, actual, buckettype='bins', buckets=10, axis=0):
        worksofchart.com
     '''
 
-    def psi(expected_array, actual_array, buckets):
+    def psi(expected_array, actual_array):
         '''Calculate the PSI for a single variable
 
         Args:
            expected_array: numpy array of original values
            actual_array: numpy array of new values, same size as expected
-           buckets: number of percentile ranges to bucket the values into
+
 
         Returns:
            psi_value: calculated PSI value
@@ -36,16 +35,6 @@ def calculate_psi(expected, actual, buckettype='bins', buckets=10, axis=0):
             input /= np.max(input) / (max - min)
             input += min
             return input
-
-
-        breakpoints = np.arange(0, buckets + 1) / (buckets) * 100
-
-        if buckettype == 'bins':
-            breakpoints = scale_range(breakpoints, np.min(expected_array), np.max(expected_array))
-        elif buckettype == 'quantiles':
-            breakpoints = np.stack([np.percentile(expected_array, b) for b in breakpoints])
-
-
 
         expected_percents = np.histogram(expected_array, breakpoints)[0] / len(expected_array)
         actual_percents = np.histogram(actual_array, breakpoints)[0] / len(actual_array)
@@ -73,10 +62,28 @@ def calculate_psi(expected, actual, buckettype='bins', buckets=10, axis=0):
 
     for i in range(0, len(psi_values)):
         if len(psi_values) == 1:
-            psi_values = psi(expected, actual, buckets)
+            psi_values = psi(expected, actual)
         elif axis == 0:
-            psi_values[i] = psi(expected[:,i], actual[:,i], buckets)
+            psi_values[i] = psi(expected[:,i], actual[:,i])
         elif axis == 1:
-            psi_values[i] = psi(expected[i,:], actual[i,:], buckets)
+            psi_values[i] = psi(expected[i,:], actual[i,:]
+                                )
 
     return(psi_values)
+
+def get_breakpoint(expected_array, buckettype = 'bins', buckets = 10):
+
+    def scale_range(input, min, max):
+        input += -(np.min(input))
+        input /= np.max(input) / (max - min)
+        input += min
+        return input
+
+    breakpoints = np.arange(0, buckets + 1) / (buckets) * 100
+
+    if buckettype == 'bins':
+        breakpoints = scale_range(breakpoints, np.min(expected_array), np.max(expected_array))
+    elif buckettype == 'quantiles':
+        breakpoints = np.stack([np.percentile(expected_array, b) for b in breakpoints])
+
+    return breakpoints
