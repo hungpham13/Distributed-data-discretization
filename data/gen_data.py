63ef27c5d72ea3df8d8d1c1d93fec039d3715e29
Hung <tonic1307@gmail.com>
2022-12-30 15:24:40 +0700

    update

--- data/gen_data.py
+++ data/gen_data.py
@@ -2,11 +2,37 @@ import random
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import seaborn as sns
-from iteration_utilities import random_combination
 from tqdm import tqdm
+from model.psi import get_breakpoint, calculate_psi
 
+
+def generate_day(length, dist, mu_range, sigma_range, value_range):
+    mu = random.uniform(mu_range[0], mu_range[1])
+
+    max_sigma = min(mu-value_range[0], value_range[1]-mu)/5
+    min_sigma = sigma_range[0] if sigma_range[0] < max_sigma else max_sigma
+    sigma = random.uniform(min_sigma, max_sigma)
+
+    if dist == 'mix':
+        d = np.random.choice(
+            ['uniform', 'logistic', 'normal', 'exponential', 'gamma'])
+    else:
+        d = dist
+
+    if (d == 'uniform'):
+        s = np.random.uniform(mu-3*3*sigma/2,
+                              mu+3*3*sigma/2, length).tolist()
+    elif (d == 'logistic'):
+        s = np.random.logistic(mu, sigma/np.pi, length).tolist()
+    elif (d == 'exponential'):
+        # sigma = random.uniform(0, 85)
+        s = (np.random.exponential(sigma/1.47, length) + mu-sigma*5).tolist()
+    elif (d == 'gamma'):
+        s = np.random.gamma((mu/sigma)**2, sigma**2/mu, length).tolist()
+    else:
+        s = np.random.normal(mu, sigma, length).tolist()
+    return [round(i) for i in s]
 
 
 def gen_nextday(prev, true, dist):
@@ -21,31 +47,12 @@ def gen_nextday(prev, true, dist):
         r = 0.1
         sigma_range = [90,]
     else:
-        r = 0.5
+        r = 0.9
         sigma_range = [45, ]
     # remove random r% of element in prev
     result = random.sample(prev, round(len(prev) * (1 - r)))
     # add r% of a new random normal distribution to result
-    mu = random.uniform(mu_range[0], mu_range[1])
-
-    max_sigma = max(
-        min(mu-value_range[0], value_range[1]-mu)/6, sigma_range[0]
-    )
-    sigma = random.uniform(sigma_range[0], max_sigma)
-
-    if dist == 'mix':
-        d = np.random.choice(['uniform', 'logistic', 'normal'])
-    else:
-        d = dist
-
-    if (d == 'uniform'):
-        s = np.random.uniform(mu-3*np.sqrt(12)*sigma/2,
-                              mu+3*np.sqrt(12)*sigma/2, len(prev)).tolist()
-    elif (d == 'logistic'):
-        s = np.random.logistic(mu, sigma*np.sqrt(3)/np.pi, len(prev)).tolist()
-    else:
-        s = np.random.normal(mu, sigma, len(prev)).tolist()
-    s = [round(i) for i in s]
+    s = generate_day(len(prev), dist, mu_range, sigma_range, value_range)
     result = result + random.sample(s, round(len(prev) * r))
     # shuffle result and return
     random.shuffle(result)
@@ -66,49 +73,38 @@ def generate_data(num_days, num_sample, dist):
     mu_range = [450, 700]
     sigma_range = [45, ]
 
-    # data = pd.DataFrame(columns=list(range(num_sample)) + ['Labels'])
-    data = np.array([list(range(num_sample)) + [-1]])
-
     # generate first day
     first_day = []
     for _ in range(bin_num):
-        mu = random.uniform(mu_range[0], mu_range[1])
-        max_sigma = max(
-            min(mu-value_range[0], value_range[1]-mu)/6, sigma_range[0])
-        sigma = random.uniform(sigma_range[0], max_sigma)
-        if dist == 'mix':
-            d = np.random.choice(['uniform', 'logistic', 'normal'])
-        else:
-            d = dist
-        if (d == 'uniform'):
-            s = np.random.uniform(mu-3*np.sqrt(12)/2*sigma, mu+3*np.sqrt(12)*sigma/2,
-                                  round(num_sample / bin_num)).tolist()
-        elif (d == 'logistic'):
-            s = np.random.logistic(mu, sigma*np.sqrt(3)/np.pi, round(
-                num_sample / bin_num)).tolist()
-        else:
-            s = np.random.normal(mu, sigma, round(
-                num_sample / bin_num)).tolist()
-        first_day.extend([round(i) for i in s])
+        s = generate_day(num_sample, dist, mu_range, sigma_range, value_range)
+        first_day.extend(s)
 
     # data.loc[0] = first_day + [0]
-    data = np.append(data, [first_day + [0]], axis=0)
+    data = np.array([first_day + [0]])
 
     # 0 is true, 1 is false
 
     prev = first_day
+    breakpoints = get_breakpoint(first_day, buckettype='bins', buckets=10)
     for day in tqdm(range(1, num_days)):
         label = np.random.choice([0, 1], p=[0.7, 0.3])
         if (label == 0):
             next = gen_nextday(prev, True, dist)
         else:
             next = gen_nextday(prev, False, dist)
+            psi = calculate_psi(expected=np.array(prev), actual=np.array(next),
+                                breakpoints=breakpoints)
+            while (psi < 0.1):
+                next = gen_nextday(prev, False, dist)
+                psi = calculate_psi(expected=np.array(prev), actual=np.array(next),
+                                    breakpoints=breakpoints)
+
         prev = next
         # data.loc[day] = next + [label]
         data = np.append(data, [next + [label]], axis=0)
 
     plt.figure()
-    for row in data[1:6, :]:
+    for row in data[0:6, :]:
         sns.kdeplot(row[:-1], color='blue' if row[-1]
                     == 0 else 'red', multiple='stack')
     plt.show()
--- model/equal_bin.py
+++ model/equal_bin.py
@@ -22,18 +22,18 @@ def predict(test, breakpoints):
     print(classification_report(label, prediction))
 
 
-def run(num_tries=1, num_train=500, num_test=200):
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    for _ in range(num_tries):
-        train, test = generate_data(num_train=num_train,
-                                    num_test=num_test,
-                                    true_ratio=0.9,
-                                    visualize=True,
-                                    num_sample=10000,
-                                    bin_num=1,
-                                    choose_config=False
-                                    )
-        data_distribution = train.drop(columns=['Label'])
-        breakpoints = get_breakpoint(data_distribution.to_numpy().reshape(-1),
-                                     buckettype='bins', buckets=10)
-        predict(test, breakpoints)
+# def run(num_tries=1, num_train=500, num_test=200):
+#     warnings.filterwarnings("ignore", category=DeprecationWarning)
+#     for _ in range(num_tries):
+#         train, test = generate_data(num_train=num_train,
+#                                     num_test=num_test,
+#                                     true_ratio=0.9,
+#                                     visualize=True,
+#                                     num_sample=10000,
+#                                     bin_num=1,
+#                                     choose_config=False
+#                                     )
+#         data_distribution = train.drop(columns=['Label'])
+#         breakpoints = get_breakpoint(data_distribution.to_numpy().reshape(-1),
+#                                      buckettype='bins', buckets=10)
+#         predict(test, breakpoints)
