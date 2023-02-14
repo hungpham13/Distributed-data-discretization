a54953e979f1b621f6393611201ee3b4a53812e0
Hung <tonic1307@gmail.com>
2023-02-14 15:30:11 +0700

    update

--- src/data_generation/local_gen_data.py
+++ src/data_generation/local_gen_data.py
@@ -1,8 +1,9 @@
-from data_generation.gen_data import generate_data
+from gen_data import generate_data
 import os
 import numpy as np
 
 ratios = [0.7, 0.8, 0.9]
+
 
 def gen_test():
     dists = ['normal', 'logistic', 'uniform', 'exponential', 'gamma']
@@ -12,12 +13,11 @@ def gen_test():
     # nums_sam = [10000000]
     # nums_day = [365*3]
     iter_bundle = [(num_samples, num_days, i, r)
-                for num_samples in nums_sam
-                for num_days in nums_day
-                for r in ratios
-                for i in range(1, 4)
-                ]
-
+                   for num_samples in nums_sam
+                   for num_days in nums_day
+                   for r in ratios
+                   for i in range(1, 4)
+                   ]
 
     for dist in dists:
         os.makedirs(f"../../test-{dist}", exist_ok=True)
@@ -27,21 +27,23 @@ def gen_test():
                 np.save(f, data)
             del data
 
+
 def gen_train():
-    nums_sam = [1000, 3000, 10000, 30000, 100000, 300000, 1000000, 3000000, 10000000]
+    nums_sam = [1000, 3000, 10000, 30000, 100000,
+                300000, 1000000, 3000000, 10000000]
     nums_day = [30, 60, 120, 365, 365*2, 365*3]
     dists = ['normal', 'logistic', 'uniform', 'exponential', 'gamma']
     # dists = ['logistic']
     # nums_sam = [1000000]
     # nums_day = [365*3]
-    iter_bundle = [(num_samples, num_days, r) 
-                        for num_samples in nums_sam 
-                        for num_days in nums_day
-                        for r in ratios
-                    ]
+    iter_bundle = [(num_samples, num_days, r)
+                   for num_samples in nums_sam
+                   for num_days in nums_day
+                   for r in ratios
+                   ]
 
     # slice_iter_bundle = iter_bundle[39:len(iter_bundle)-3]
-    print("Going to generate",len(iter_bundle),"distributions")
+    print("Going to generate", len(iter_bundle), "distributions")
 
     for dist in dists:
         os.makedirs(f"/work/{dist}", exist_ok=True)
