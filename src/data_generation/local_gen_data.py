684910de098f3d8f6ed629c91d21352619945a28
Hung <tonic1307@gmail.com>
2023-02-24 12:11:28 +0700

    update local_gen make dir

--- src/data_generation/local_gen_data.py
+++ src/data_generation/local_gen_data.py
@@ -8,7 +8,8 @@ output_folder = '/work'
 
 def gen_test():
     dists = ['normal', 'logistic', 'uniform', 'exponential', 'gamma']
-    nums_sam = [10000, 100000, 1000000, 5000000, 10000000]
+    # nums_sam = [10000, 100000, 1000000, 5000000, 10000000]
+    nums_sam = [10000, 100000, 1000000]
     nums_day = [183, 365, 365*3]
     # dists = ['normal']
     # nums_sam = [10000000]
@@ -25,6 +26,7 @@ def gen_test():
     print("Going to generate", len(slice_iter_bundle), "distributions")
     commands = []
     for dist, num_samples, num_days, index, r in slice_iter_bundle:
+        os.makedirs(f"{output_folder}/test-{dist}", exist_ok=True)
         commands.append(
             f"python3 data_generation/gen_data.py --days {num_days} --samples {num_samples} --dist {dist} --ratio {r} --output {output_folder}/test-{dist}/test_{index}_{dist}_{num_days}_days_{num_samples}_samples_{int(r*100)}.npy")
 
@@ -33,12 +35,13 @@ def gen_test():
 
 
 def gen_train():
-    nums_sam = [1000, 3000, 10000, 30000, 100000,
-                300000, 1000000, 3000000, 10000000]
+    # nums_sam = [1000, 3000, 10000, 30000, 100000,
+    #             300000, 1000000, 3000000, 10000000]
     nums_day = [30, 60, 120, 365, 365*2, 365*3]
     dists = ['normal', 'logistic', 'uniform', 'exponential', 'gamma']
     # dists = ['logistic']
     # nums_sam = [1000000]
+    nums_sam = [1000, 3000, 10000, 30000, 100000, 300000, 1000000]
     # nums_day = [365*3]
     iter_bundle = [(dist, num_samples, num_days, r)
                    for dist in dists
@@ -52,6 +55,7 @@ def gen_train():
 
     commands = []
     for dist, num_samples, num_days, r in slice_iter_bundle:
+        os.makedirs(f"{output_folder}/{dist}", exist_ok=True)
         commands.append(
             f"python3 data_generation/gen_data.py --days {num_days} --samples {num_samples} --dist {dist} --ratio {r} --output {output_folder}/{dist}/{dist}_{num_days}_days_{num_samples}_samples_{int(r*100)}.npy")
 
